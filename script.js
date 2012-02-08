ed27f339b3a0ca2e7201b400ac3c3db6a2051207
Marco <TubeToTV@gmail.com>
2012-02-08 13:50:27 +0100

    tube 0.5.3

---
+++ script.js
@@ -0,0 +1,87 @@
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+// how many buttons?
+if (request.method == "getStatus")
+       sendResponse({status: localStorage["xbmc_count"]});
+
+if (request.method == "getImage")
+       sendResponse({image: localStorage["imageNo"]});
+
+// without settings
+if (request.play==1337){
+       alert("Please enter your XBMC IP in the options menu first.");
+       chrome.tabs.create({'url':
+chrome.extension.getURL("options.html")},function(){});
+}
+
+if (typeof(request.play)=='number'){
+
+var x=request.play;
+var str;
+var xbmc_ip = JSON.parse(localStorage["xbmc_ip"])[x].value;
+var xbmc_user = JSON.parse(localStorage["xbmc_user"])[x].value;
+var xbmc_pw = JSON.parse(localStorage["xbmc_pw"])[x].value;
+var myVideo;
+
+if (xbmc_ip){
+chrome.tabs.getSelected(null,function(tab) {
+       call_req(tab.url);
+});
+
+function call_req(str) {
+       xmlplay=new XMLHttpRequest();
+	   xmlversion=new XMLHttpRequest();
+
+       //need username + pw?
+       if (xbmc_user){
+               xmlplay.open('POST','http://' + xbmc_user + ':' + xbmc_pw + '@' + xbmc_ip + '/jsonrpc', true);
+               xmlversion.open('POST','http://' + xbmc_user + ':' + xbmc_pw + '@' + xbmc_ip + '/jsonrpc', true);
+       }else{
+               xmlplay.open('POST','http://' + xbmc_ip + '/jsonrpc', true);
+               xmlversion.open('POST','http://' + xbmc_ip + '/jsonrpc', true);
+		}
+       //if (xmlplay.readyState == 1)
+       //{
+               //xmlplay.setRequestHeader('Content-type','application/json');
+               //regular "new" tube link or additional url parameter
+               if (str.match('&') && (str.indexOf("v=") < str.indexOf("&")))
+						myVideo   = str.slice((str.indexOf("v=")+2),(str.indexOf("&", str.indexOf("v=") + 2)));
+               else
+                       myVideo   = str.slice((str.indexOf("v=")+2));
+
+					   //console.log(myVideo);
+               // send request to xbmc
+               json_version = '{ "jsonrpc": "2.0", "method": "JSONRPC.Version", "id": 1 }';
+							   
+               xmlversion.send(json_version);
+			   xmlversion.onreadystatechange = function () {
+			   if (xmlversion.readyState != 4) return;
+               if (xmlversion.responseText.match('"version":3'))
+                       //new json 2011/10/12
+					   json_play = '{"jsonrpc": "2.0", "method": "Player.Open", "params":{"item": {"file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '" }}, "id" : "1"}'
+               else
+                       json_play = '{"jsonrpc": "2.0", "method": "XBMC.Play", "params":{"file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '" }, "id" : "1"}';
+               xmlplay.send(json_play);
+			   }
+       //}
+       //else
+//                      alert("Can't connect to XBMC!";
+
+       xmlplay.onreadystatechange = function () {
+       if (xmlplay.readyState != 4) return;
+               console.log('responseText: ' + xmlplay.responseText);
+               if ((xmlplay.responseText.match('"result":"OK"')) || (xmlplay.responseText.match('"result" : "OK"')))
+                       sendResponse(true);
+               else
+                       sendResponse(false);
+       }
+}
+ //sendResponse("Done");
+ //console.log(xmlplay.responseText);
+
+}
+else{
+alert("Please enter your XBMC IP in the options menu first.");
+chrome.tabs.create({'url':chrome.extension.getURL("options.html")},function(){});
+}
+}}
+);
---
+++ ytube.js
@@ -0,0 +1,87 @@
+// ask extension how many buttons should be implemented
+chrome.extension.sendRequest({method: "getStatus"}, function(response) {
+	var xbmc_count = response.status;
+	//console.log("count: "+xbmc_count);
+	
+chrome.extension.sendRequest({method: "getImage"}, function(response) {
+	if (response.image)
+		var imageNo = response.image;
+	else
+		var imageNo = xx;
+	//console.log("image: "+imageNo);
+	
+var imgURL = chrome.extension.getURL("playbtn" + imageNo + ".png");
+
+var cssURL = chrome.extension.getURL("yttt.css");
+// normal youtube UI
+var ni = document.getElementById('watch-headline-user-info');
+//css
+var ncss = document.getElementById('www-core-css');
+
+var css_ttt = document.createElement('link');
+css_ttt.setAttribute('rel', 'stylesheet');
+css_ttt.setAttribute('href', cssURL);
+ncss.appendChild(css_ttt);
+
+// first time only - no settings saved yet
+if (!xbmc_count){
+	var playbtn = document.createElement('button');
+	playbtn.setAttribute('id','TubeToTV');
+	if (imageNo=="xx"){
+		playbtn.setAttribute('class','start yt-uix-button');
+		playbtn.innerHTML = '<a name="TubeToTV" selector="1337" id="masthead-nav a" class="btn_ttt">XBMC</a>';}
+	else
+		playbtn.innerHTML = '<img name="TubeToTV" selector="1337" id="btn_ttt" class="btn_ttt" src="' + imgURL + '"/>';
+	ni.appendChild(playbtn);
+}
+
+for(y = 0; y < xbmc_count; y++){
+	var playbtn = document.createElement('button');
+	playbtn.setAttribute('id','TubeToTV');
+		
+	// special case without number for only 1 button
+	if (xbmc_count>1)
+		// text or image button
+		if (imageNo=="xx"){
+			playbtn.setAttribute('class','start yt-uix-button');
+			playbtn.innerHTML = '<span class="yt-uix-button-content" name="TubeToTV">Send to XBMC</span></button>';}
+		else
+			playbtn.innerHTML = '<span class="xbmc_no">'+ (y+1) +'</span><img name="TubeToTV" selector="' + y + '" id="btn_ttt' + y + '" class="btn_ttt" src="' + imgURL + '"/>';
+	else
+		if (imageNo=="xx"){
+			playbtn.setAttribute('class','start yt-uix-button');
+			playbtn.innerHTML = '<span class="yt-uix-button-content" name="TubeToTV">Send to XBMC</span>';}
+		else
+			playbtn.innerHTML = '<img name="TubeToTV" id="btn_ttt' + y + '" class="btn_ttt" src="' + imgURL + '"/>';
+			
+	ni.appendChild(playbtn);
+}
+	// collect all buttons and wait for a click
+    var xbuttons = document.getElementsByName('TubeToTV');
+		for (var i=0; i<xbuttons.length; i++){
+			xbuttons[i].onclick = function(){playDetected();};
+			}
+        
+function playDetected(){
+		var e = window.event;
+		var player = document.getElementById('movie_player');
+		
+		// fade-in-out of button -- looks like it is working
+		e.target.setAttribute('style', '-webkit-animation-name: blink;');
+		// send request to play the video
+        chrome.extension.sendRequest({play: Number(e.target.getAttribute("selector"))}, function(response) {
+		// connection successful?
+		if(response){
+			console.log("Sent to XBMC: "+response);
+			e.target.setAttribute('style','opacity: 1;');
+			player.stopVideo();
+			}
+		/*else{
+			console.log("Sent to XBMC: "+response);
+			e.target.setAttribute('style','opacity: 1;');
+			alert("Check your connection info in the options!");
+			}*/
+		}); 
+		}
+
+});});
