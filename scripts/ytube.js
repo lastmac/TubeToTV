7b4098eab621c6aff16fd8c3604c5aaf6093c5ee
Marco <TubeToTV@gmail.com>
2012-09-11 17:58:08 +0200

    Signed-off-by: Marco <TubeToTV@gmail.com>

--- scripts/ytube.js
+++ scripts/ytube.js
@@ -1,28 +1,4 @@
 // ask extension how many buttons should be implemented
-function Notifier() {}
- 
-// Request permission for this page to send notifications. If allowed,
-// calls function "cb" with "true" as the first argument.
-Notifier.prototype.RequestPermission = function(cb) {
-  window.webkitNotifications.requestPermission(function() {
-    if (cb) { cb(window.webkitNotifications.checkPermission() == 0); }
-  });
-}
- 
-// Popup a notification with icon, title, and body. Returns false if
-// permission was not granted.
-Notifier.prototype.Notify = function(icon, title, body) {
-  if (window.webkitNotifications.checkPermission() == 0) {
-    var popup = window.webkitNotifications.createNotification(
-      icon, title, body);
-    popup.show();
-    return true;
-  }
- 
-  return false;
-}
-
-
 chrome.extension.sendRequest({method: "getStatus"}, function(response) {
 	var xbmc_count = response.status;
 	console.log("count: "+xbmc_count);
