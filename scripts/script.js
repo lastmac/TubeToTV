f089c30f22233d5fa78531e460b4004ef0f2677d
Marco <TubeToTV@gmail.com>
2013-03-05 12:07:15 +0100

    added the possibility to name the buttons. Thanks to Joshua for this!

--- scripts/script.js
+++ scripts/script.js
@@ -1,174 +1,210 @@
 chrome.extension.onRequest.addListener(
-	function(request, sender, sendResponse) {
-		// how many buttons?
-		if (request.method == "getStatus"){
-			if(localStorage["xbmc_count"])
-				sendResponse({status: localStorage["xbmc_count"]});
-			else
-				sendResponse({status: 0});
-		}
-		if (request.method == "getImage")
-			   sendResponse({image: localStorage["imageNo"]});
+function (request, sender, sendResponse) {
+	// how many buttons?
+	if (request.method == "getStatus") {
+		if (localStorage["xbmc_count"]) sendResponse({
+			status: localStorage["xbmc_count"],
+			name: localStorage["xbmc_name"]
+		});
+		else sendResponse({
+			status: 0,
+			name: "[]"
+		});
+	}
+	if (request.method == "getImage") sendResponse({
+		image: localStorage["imageNo"]
+	});
 
-		// without settings
-		if (request.play==1337){
-			   alert("Please enter your XBMC IP in the options menu first.");
-			   chrome.tabs.create({'url':
-			   chrome.extension.getURL("/options.html")},function(){});
-		}
+	// without settings
+	if (request.play == 1337) {
+		alert("Please enter your XBMC IP in the options menu first.");
+		chrome.tabs.create({
+			'url': chrome.extension.getURL("/options.html")
+		}, function () {});
+	}
 
-		if (typeof(request.add)=='number'){
+	if (typeof (request.add) == 'number') {
 		console.log("in add...");
-		var x=request.add;
+		var x = request.add;
 		var str;
 		var xbmc_ip = JSON.parse(localStorage["xbmc_ip"])[x].value;
 		var xbmc_user = JSON.parse(localStorage["xbmc_user"])[x].value;
 		var xbmc_pw = JSON.parse(localStorage["xbmc_pw"])[x].value;
 		var myVideo;
 
-		if (xbmc_ip){
-		chrome.tabs.getSelected(null,function(tab) {
-			   call_req(tab.url);
-		});
+		if (xbmc_ip) {
+			chrome.tabs.getSelected(null, function (tab) {
+				call_req(tab.url);
+			});
 
-		function call_req(str) {
+			function call_req(str) {
+				getVideo(str, function (callback) {
+					myVideo = callback
+				});
+				console.log("in add video=" + myVideo);
+				json_version = '{ "jsonrpc": "2.0", "method": "JSONRPC.Version", "id": 1 }';
+				connect(xbmc_user, xbmc_pw, xbmc_ip, function (callback) {
+					xmlplay = callback;
+				});
+				connect(xbmc_user, xbmc_pw, xbmc_ip, function (callback) {
+					xmlversion = callback;
+				});
+				xmlversion.send(json_version);
 
-					   getVideo(str, function(callback){myVideo=callback});
-					   console.log("in add video=" + myVideo);
-					   json_version = '{ "jsonrpc": "2.0", "method": "JSONRPC.Version", "id": 1 }';
-					   connect(xbmc_user, xbmc_pw, xbmc_ip, function(callback){xmlplay=callback;});
-					   connect(xbmc_user, xbmc_pw, xbmc_ip, function(callback){xmlversion=callback;});
-					   xmlversion.send(json_version);
+				xmlversion.onreadystatechange = function () {
+					if (xmlversion.readyState != 4) return;
 
-					   xmlversion.onreadystatechange = function () {
-					   if (xmlversion.readyState != 4) return;
-					   if ((xmlversion.responseText.match('"version":4'))||(xmlversion.responseText.match('"version": 4')))
-							   //new json 2012/03/08 - Eden
-								json_add = '{"jsonrpc": "2.0", "method": "Playlist.Add", "params":{"playlistid":1,"item":{ "file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '"}}, "id" : 1}'
-					   else if ((xmlversion.responseText.match('"version":5'))||(xmlversion.responseText.match('"version": 5')))
-							   //new json 2012/05/09 - pre Frodo
-								json_add = '{"jsonrpc": "2.0", "method": "Playlist.Add", "params":{"playlistid":1,"item":{ "file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '"}}, "id" : 1}'					   
-						else if (xmlversion.responseText.match('{"version":{"major":6'))
-							   //new json 2012/12 - RC2 Frodo
-								json_add = '{"jsonrpc": "2.0", "method": "Playlist.Add", "params":{"playlistid":1,"item":{ "file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '"}}, "id" : 1}'
-					   xmlplay.send(json_add);
-						notify = webkitNotifications.createNotification("images/browseraction.png","","Added to Playlist");
-						   notify.show();
-						   setTimeout(function(){
-						   notify.cancel();
-						   }, '2000');
-					   }
+					console.log("JSON Version: " + xmlversion.responseText);
+					if ((xmlversion.responseText.match('"version":4')) || (xmlversion.responseText.match('"version": 4'))) {
+						// json 2012/03/08 - Eden
+						json_add = '{"jsonrpc": "2.0", "method": "Playlist.Add", "params":{"playlistid":1,"item":{ "file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '"}}, "id" : 1}'
+					}
+					else if ((xmlversion.responseText.match('"version":5')) || (xmlversion.responseText.match('"version": 5'))) {
+						// json 2012/05/09 - pre Frodo
+						json_add = '{"jsonrpc": "2.0", "method": "Playlist.Add", "params":{"playlistid":1,"item":{ "file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '"}}, "id" : 1}'
+					}
+					else if (xmlversion.responseText.match('{"version":{"major":6')) {
+						// json 2012/12 - RC2 Frodo
+						json_add = '{"jsonrpc": "2.0", "method": "Playlist.Add", "params":{"playlistid":1,"item":{ "file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '"}}, "id" : 1}'
+					}
+					else if (!xmlversion.responseText) {
+						notify = webkitNotifications.createNotification("images/browseraction.png", "", "Connection Error");
+						notify.show();
+						setTimeout(function () {
+							notify.cancel();
+						}, '3000');
+						return;
+					}
 
-		//                      alert("Can't connect to XBMC!";
+					xmlplay.send(json_add);
+					notify = webkitNotifications.createNotification("images/browseraction.png", "", "Added to Playlist");
+					notify.show();
+					setTimeout(function () {
+						notify.cancel();
+					}, '2000');
+				}
 
-			   xmlplay.onreadystatechange = function () {
-			   if (xmlplay.readyState != 4) return;
-					   console.log('responseText: ' + xmlplay.responseText);
-					   if ((xmlplay.responseText.match('"result":"OK"')) || (xmlplay.responseText.match('"result" : "OK"'))){
-							   sendResponse(true);
-							   }
-					   else{
-							   sendResponse(false);
-							   notify = webkitNotifications.createNotification("images/browseraction.png","","Something went wrong");
-							   notify.show();
-							   setTimeout(function(){
-							   notify.cancel();
-							   }, '4000');
-							   }
-			   }
+				// alert("Can't connect to XBMC!";
+
+				xmlplay.onreadystatechange = function () {
+					if (xmlplay.readyState != 4) return;
+
+					console.log('responseText: ' + xmlplay.responseText);
+					if ((xmlplay.responseText.match('"result":"OK"')) || (xmlplay.responseText.match('"result" : "OK"'))) {
+						sendResponse(true);
+					} else {
+						sendResponse(false);
+						notify = webkitNotifications.createNotification("images/browseraction.png", "", "Something went wrong");
+						notify.show();
+						setTimeout(function () {
+							notify.cancel();
+						}, '4000');
+					}
+				}
+			}
+			// sendResponse("Done");
 		}
-		 //sendResponse("Done");
+		else {
+			// alert("Please enter your XBMC IP in the options menu first.");
+			chrome.tabs.create({
+				'url': chrome.extension.getURL("/options.html")
+			}, function () {});
+		}
+	}
 
-		}
-		else{
-		alert("Please enter your XBMC IP in the options menu first.");
-		chrome.tabs.create({'url':chrome.extension.getURL("/options.html")},function(){});
-		}
-		}
-
-		if (typeof(request.play)=='number'){
+	if (typeof (request.play) == 'number') {
 		console.log("in play..." + request.play);
-		var x=request.play;
+		var x = request.play;
 		var str;
 		var xbmc_ip = JSON.parse(localStorage["xbmc_ip"])[x].value;
 		var xbmc_user = JSON.parse(localStorage["xbmc_user"])[x].value;
 		var xbmc_pw = JSON.parse(localStorage["xbmc_pw"])[x].value;
 		var myVideo;
 
-		if (xbmc_ip){
-		chrome.tabs.getSelected(null,function(tab) {
-			   call_reqp(tab.url);
-		});
+		if (xbmc_ip) {
+			chrome.tabs.getSelected(null, function (tab) {
+				call_reqp(tab.url);
+			});
 
-		function call_reqp(str) {
-			   getVideo(str, function(callback){myVideo=callback});
-			   json_version = '{ "jsonrpc": "2.0", "method": "JSONRPC.Version", "id": 1 }';
-			   connect(xbmc_user, xbmc_pw, xbmc_ip, function(callback){xmlplay=callback;});
-			   connect(xbmc_user, xbmc_pw, xbmc_ip, function(callback){xmlversion=callback;});
-			   xmlversion.send(json_version);
-			   
-			   xmlversion.onreadystatechange = function () {
-			   if (xmlversion.readyState != 4) return;
+			function call_reqp(str) {
+				getVideo(str, function (callback) {
+					myVideo = callback
+				});
+				json_version = '{ "jsonrpc": "2.0", "method": "JSONRPC.Version", "id": 1 }';
+				connect(xbmc_user, xbmc_pw, xbmc_ip, function (callback) {
+					xmlplay = callback;
+				});
+				connect(xbmc_user, xbmc_pw, xbmc_ip, function (callback) {
+					xmlversion = callback;
+				});
+				xmlversion.send(json_version);
 
-			   console.log("JSON Version: "+xmlversion.responseText);
-			   if ((xmlversion.responseText.match('"version":4'))||(xmlversion.responseText.match('"version": 4')))
-					   //new json 2012/03/08 - Eden
-						json_play = '[{"jsonrpc": "2.0", "method": "Playlist.Clear", "params":{"playlistid":1}, "id": 1},{"jsonrpc": "2.0", "method": "Playlist.Add", "params":{"playlistid":1,"item":{ "file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '"} }, "id": 1},{"jsonrpc": "2.0", "method": "Player.Open", "params":{"item":{"playlistid":1, "position" : 0}}, "id": 1}]'				
-			   else if ((xmlversion.responseText.match('"version":5'))||(xmlversion.responseText.match('"version": 5')))
-					   //new json 2012/05/09 - pre Frodo
-						json_play = '[{"jsonrpc": "2.0", "method": "Playlist.Clear", "params":{"playlistid":1}, "id": 1},{"jsonrpc": "2.0", "method": "Playlist.Add", "params":{"playlistid":1,"item":{ "file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '"} }, "id": 1},{"jsonrpc": "2.0", "method": "Player.Open", "params":{"item":{"playlistid":1, "position" : 0}}, "id": 1}]'							   
-    			else if (xmlversion.responseText.match('{"version":{"major":6'))
-					   //new json 2012/12 - RC2 Frodo
-						json_play = '[{"jsonrpc": "2.0", "method": "Playlist.Clear", "params":{"playlistid":1}, "id": 1},{"jsonrpc": "2.0", "method": "Playlist.Add", "params":{"playlistid":1,"item":{ "file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '"} }, "id": 1},{"jsonrpc": "2.0", "method": "Player.Open", "params":{"item":{"playlistid":1, "position" : 0}}, "id": 1}]'				
-				else if (!xmlversion.responseText)
-				{	
-				   notify = webkitNotifications.createNotification("images/browseraction.png","","Connection Error");
-				   notify.show();
-				   setTimeout(function(){
-				   notify.cancel();
-				   }, '3000');		
-				   return;
-				   }
-			   else
-					   json_play = '{"jsonrpc": "2.0", "method": "XBMC.Play", "params":{"file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '" }, "id" : "1"}';
-				console.log("JSON Play: "+json_play);
-				xmlplay.send(json_play);
-				   notify = webkitNotifications.createNotification("images/browseraction.png","","Send to XBMC");
-				   notify.show();
-				   setTimeout(function(){
-				   notify.cancel();
-				   }, '2000');				
-			   }
+				xmlversion.onreadystatechange = function () {
+					if (xmlversion.readyState != 4) return;
 
-		//  alert("Can't connect to XBMC!";
+					console.log("JSON Version: " + xmlversion.responseText);
+					if ((xmlversion.responseText.match('"version":4')) || (xmlversion.responseText.match('"version": 4'))) {
+						// json 2012/03/08 - Eden
+						json_play = '[{"jsonrpc": "2.0", "method": "Playlist.Clear", "params":{"playlistid":1}, "id": 1},{"jsonrpc": "2.0", "method": "Playlist.Add", "params":{"playlistid":1,"item":{ "file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '"} }, "id": 1},{"jsonrpc": "2.0", "method": "Player.Open", "params":{"item":{"playlistid":1, "position" : 0}}, "id": 1}]'
+					}
+					else if ((xmlversion.responseText.match('"version":5')) || (xmlversion.responseText.match('"version": 5'))) {
+						// json 2012/05/09 - pre Frodo
+						json_play = '[{"jsonrpc": "2.0", "method": "Playlist.Clear", "params":{"playlistid":1}, "id": 1},{"jsonrpc": "2.0", "method": "Playlist.Add", "params":{"playlistid":1,"item":{ "file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '"} }, "id": 1},{"jsonrpc": "2.0", "method": "Player.Open", "params":{"item":{"playlistid":1, "position" : 0}}, "id": 1}]'
+					}
+					else if (xmlversion.responseText.match('{"version":{"major":6')) {
+						// json 2012/12 - RC2 Frodo
+						json_play = '[{"jsonrpc": "2.0", "method": "Playlist.Clear", "params":{"playlistid":1}, "id": 1},{"jsonrpc": "2.0", "method": "Playlist.Add", "params":{"playlistid":1,"item":{ "file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '"} }, "id": 1},{"jsonrpc": "2.0", "method": "Player.Open", "params":{"item":{"playlistid":1, "position" : 0}}, "id": 1}]'
+					}
+					else if (!xmlversion.responseText) {
+						notify = webkitNotifications.createNotification("images/browseraction.png", "", "Connection Error");
+						notify.show();
+						setTimeout(function () {
+							notify.cancel();
+						}, '3000');
+						return;
+					}
+					else {
+						json_play = '{"jsonrpc": "2.0", "method": "XBMC.Play", "params":{"file" : "plugin://plugin.video.youtube/?action=play_video&videoid=' + myVideo + '" }, "id" : "1"}';
+					}
 
-			   xmlplay.onreadystatechange = function () {
-			   if (xmlplay.readyState != 4) return;
-					   console.log('responseText: ' + xmlplay.responseText);
-					   if ((xmlplay.responseText.match('"result":"OK"')) || (xmlplay.responseText.match('"result" : "OK"'))){
-							   sendResponse(true);
-							   }
-					   else{
-							   sendResponse(false);
-							   notify = webkitNotifications.createNotification("images/browseraction.png","","Something went wrong");
-							   notify.show();
-							   setTimeout(function(){
-							   notify.cancel();
-							   }, '4000');
-							   }
-			   }
-		}
-		 //sendResponse("Done");
-		 //console.log(xmlplay.responseText);
+					console.log("JSON Play: " + json_play);
+					xmlplay.send(json_play);
+					notify = webkitNotifications.createNotification("images/browseraction.png", "", "Sent to XBMC");
+					notify.show();
+					setTimeout(function () {
+						notify.cancel();
+					}, '2000');
+				}
+
+				//  alert("Can't connect to XBMC!";
+
+				xmlplay.onreadystatechange = function () {
+					if (xmlplay.readyState != 4) return;
+					console.log('responseText: ' + xmlplay.responseText);
+					if ((xmlplay.responseText.match('"result":"OK"')) || (xmlplay.responseText.match('"result" : "OK"'))) {
+						sendResponse(true);
+					} else {
+						sendResponse(false);
+						notify = webkitNotifications.createNotification("images/browseraction.png", "", "Something went wrong");
+						notify.show();
+						setTimeout(function () {
+							notify.cancel();
+						}, '4000');
+					}
+				}
+			}
+			//sendResponse("Done");
+			//console.log(xmlplay.responseText);
 
 		}
-		else{
-		alert("Please enter your XBMC IP in the options menu first.");
-		chrome.tabs.create({'url':chrome.extension.getURL("/options.html")},function(){});
+		else {
+			alert("Please enter your XBMC IP in the options menu first.");
+			chrome.tabs.create({
+				'url': chrome.extension.getURL('/options.html')
+			}, function () {});
 		}
-		}
-		}
-);
+	}
+});
 
 function getVideo(str, callback){
    if (str.match('&') && (str.indexOf("v=") < str.indexOf("&")))
@@ -177,7 +213,7 @@ function getVideo(str, callback){
 		str   = str.slice((str.indexOf("v=")+2));
 	console.log("Video: "+str);
 	callback(str);
-}
+	}
 
 function connect(usr, pw, ip, callback){
 	xmlrequest=new XMLHttpRequest();
--- scripts/ytube.js
+++ scripts/ytube.js
@@ -1,128 +1,156 @@
 // ask extension how many buttons should be implemented
-chrome.extension.sendRequest({method: "getStatus"}, function(response) {
+chrome.extension.sendRequest({
+	method: "getStatus"
+}, function (response) {
 	var xbmc_count = response.status;
-	console.log("count: "+xbmc_count);
+	var xbmc_name = response.name;
+	if (!xbmc_name)
+		xbmc_name = '[{"value":""}]'
+	console.log("count: " + xbmc_count);
+	console.log("names: " + xbmc_name);
 
-//youtube layout until 10/2012
-var ytbcss_old = "start yt-uix-button yt-uix-button-default"	
-//layout from 10/2012
-var ytbcss_new = "yt-uix-button yt-uix-button-text"		
-	
-var cssURL = chrome.extension.getURL("/css/yttt.css");
-// normal youtube UI
-var ni = document.getElementById('watch7-headline');
-//css
-var ncss = document.getElementById('www-core-css');
+	//youtube layout until 10/2012
+	var ytbcss_old = "start yt-uix-button yt-uix-button-default"
+	//layout from 10/2012
+	// var ytbcss_new = "yt-uix-button yt-uix-button-text"
+	var ytbcss_new = "yt-uix-button yt-uix-button-text yt-uix-tooltip"
 
-//new or old youtube layout?
-if (!ni){
+	var cssURL = chrome.extension.getURL("/css/yttt.css");
+	// normal youtube UI
+	var ni = document.getElementById('watch7-headline');
+	//css
+	var ncss = document.getElementById('www-core-css');
+
+	//new or old youtube layout?
+	if (!ni) {
 		var ni = document.getElementById('watch-headline-user-info');
-		ytbcss=ytbcss_old;
-		}
-		else
-		{
-		ytbcss=ytbcss_new;
-		}
+		ytbcss = ytbcss_old;
+	}
+	else {
+		ytbcss = ytbcss_new;
+	}
 
-		
-var css_ttt = document.createElement('link');
-css_ttt.setAttribute('rel', 'stylesheet');
-css_ttt.setAttribute('href', cssURL);
-//ncss.appendChild(css_ttt);
 
-// first time only - no settings saved yet
-if (!xbmc_count){
-	var playbtn = document.createElement('button');
-	playbtn.setAttribute('id','TubeToTV');
-	playbtn.setAttribute('name','TubeToTV');
-	playbtn.setAttribute('class',ytbcss);
-	playbtn.innerHTML = '<span  selector="1337" class="yt-uix-button-content">XBMC Settings</span>';
-	ni.appendChild(playbtn);
-}
+	var css_ttt = document.createElement('link');
+	css_ttt.setAttribute('rel', 'stylesheet');
+	css_ttt.setAttribute('href', cssURL);
+	// ncss.appendChild(css_ttt);
 
-for(y = 0; y < xbmc_count; y++){
-	var playbtn = document.createElement('button');
-	var addbtn = document.createElement('button');
-	playbtn.setAttribute('id','TubeToTV');
-	playbtn.setAttribute('name','TubeToTV');
+	// first time only - no settings saved yet
+	if (!xbmc_count) {
+		var playbtn = document.createElement('button');
+		playbtn.setAttribute('id', 'TubeToTV');
+		playbtn.setAttribute('name', 'TubeToTV');
+		playbtn.setAttribute('class', ytbcss);
+		playbtn.setAttribute('title', 'Configure TubeToTV with your XBMC settings');
+		playbtn.innerHTML = '<span selector="1337" class="yt-uix-button-content">XBMC Settings</span>';
+		ni.appendChild(playbtn);
+	}
 
-	// special case without number for only 1 button
-	if (xbmc_count>1)
+	for (y = 0; y < xbmc_count; y++) {
+		var playbtn = document.createElement('button');
+		var addbtn = document.createElement('button');
+		playbtn.setAttribute('id', 'TubeToTV');
+		playbtn.setAttribute('name', 'TubeToTV');
+		playbtn.setAttribute('class', ytbcss);
+		playbtn.setAttribute('style', 'margin-right: 10px;');
+		addbtn.setAttribute('class', ytbcss);
+		addbtn.setAttribute('name', 'TubeToTV_add');
+
+		// special case without number for only 1 button
 		// text / image button scrapped
-		{
-			playbtn.setAttribute('class',ytbcss);
-			playbtn.setAttribute('name','TubeToTV');
-			playbtn.setAttribute('selector',y);
-			addbtn.setAttribute('class',ytbcss);
-			addbtn.setAttribute('name','TubeToTV_add');
-			addbtn.setAttribute('selector',y);
-			
-			playbtn.innerHTML = '<span selector="' + y + '">XBMC</span>';
-			addbtn.innerHTML = '<span selector="' + y + '">+</span>';
+		if (xbmc_count > 1) {
+			var default_name = JSON.parse(xbmc_name)[y].value;
+			if (!default_name)
+				default_name = 'XBMC';
+
+			playbtn.setAttribute('title', 'Start playing this video on ' + default_name);
+			playbtn.setAttribute('selector', y);
+			addbtn.setAttribute('title', 'Add to ' + default_name + ' playlist');
+			addbtn.setAttribute('selector', y);
+
+			playbtn.innerHTML = '<span class="yt-uix-button-content" selector="' + y + '">' + default_name + '</span>';
+			addbtn.innerHTML = '<span class="yt-uix-button-content" selector="' + y + '">&#10010;</span>';
+		}
+		else {
+			var default_name = JSON.parse(xbmc_name)[0].value;
+			if (!default_name)
+				default_name = 'Send to XBMC';
+
+			playbtn.setAttribute('title', 'Start playing this video on ' + default_name);
+			addbtn.setAttribute('title', 'Add to ' + default_name + ' playlist');
+
+			playbtn.innerHTML = '<span class="yt-uix-button-content">' + default_name + '</span>';
+			addbtn.innerHTML = '<span class="yt-uix-button-content">&#10010;</span>';
 		}
 
-	else
-		{
-			//playbtn.setAttribute('class','start yt-uix-button yt-uix-button-default');
-			playbtn.setAttribute('class',ytbcss);
-			playbtn.setAttribute('name','TubeToTV');
-			addbtn.setAttribute('class',ytbcss);
-			addbtn.setAttribute('name','TubeToTV_add');
-			
-			playbtn.innerHTML = '<span>Send to XBMC</span>';
-			addbtn.innerHTML = '<span>+</span>';
-		}
-
-
-	ni.appendChild(addbtn);
-	ni.appendChild(playbtn);
-}
+		ni.appendChild(addbtn);
+		ni.appendChild(playbtn);
+	}
 	// collect all PLAY buttons and wait for a click
-    var playButtons = document.getElementsByName('TubeToTV');
-		for (var i=0; i<playButtons.length; i++){
-			playButtons[i].onclick = function(){playDetected();};
-			}	
-	
+	var playButtons = document.getElementsByName('TubeToTV');
+	for (var i = 0; i < playButtons.length; i++) {
+		playButtons[i].onclick = function () {
+			playDetected();
+		};
+	}
+
 	// collect all ADD buttons and wait for a click
-    var addButtons = document.getElementsByName('TubeToTV_add');
-		for (var i=0; i<addButtons.length; i++){
-			addButtons[i].onclick = function(){addDetected();};
-			}
-        
-function playDetected(){
+	var addButtons = document.getElementsByName('TubeToTV_add');
+	for (var i = 0; i < addButtons.length; i++) {
+		addButtons[i].onclick = function () {
+			addDetected();
+		};
+	}
+
+	function playDetected() {
 		console.log("playDetected");
 		var e = window.event;
 		var player = document.getElementById('movie_player');
+		console.log("selector: " + e.target.getAttribute('selector'));
 
 		// send request to play the video
-        chrome.extension.sendRequest({play: Number(e.target.getAttribute("selector"))}, function(response) {
-		// connection successful?
-		if(response){
-			console.log("Sent to XBMC: "+response);
-			player.stopVideo();
+		chrome.extension.sendRequest({
+			play: Number(e.target.getAttribute('selector'))
+		}, function (response) {
+			// connection successful?
+			if (response) {
+				console.log('Sent to XBMC: ' + response);
+				try{
+					player.stopVideo();
+					}
+				catch(err){
+					console.log("HTML5, can't stop video right now")
+					}
 			}
-		/*else{
-			console.log("Sent to XBMC: "+response);
-			e.target.setAttribute('style','opacity: 1;');
-			alert("Check your connection info in the options!");
+			/*else {
+				console.log("Sent to XBMC: " + response);
+				e.target.setAttribute('style', 'opacity: 1;');
+				alert("Check your connection info in the options!");
 			}*/
-		}); 
-		}
-		
-function addDetected(){
-		console.log("addDetected");
+		});
+	}
+
+	function addDetected() {
+		console.log('addDetected');
 		var e = window.event;
 		var player = document.getElementById('movie_player');
 
 		// send request to play the video
-        chrome.extension.sendRequest({add: Number(e.target.getAttribute("selector"))}, function(response) {
-		
-		// connection successful?
-		if(response){
-			console.log("Sent to XBMC: "+response);
-			player.stopVideo();
+		chrome.extension.sendRequest({
+			add: Number(e.target.getAttribute('selector'))
+		}, function (response) {
+
+			// connection successful?
+			if (response) {
+				console.log('Sent to XBMC: ' + response);
+				try{
+					player.stopVideo();
+					}
+				catch(err){
+					console.log("HTML5, can't stop video right now")
+					}
 			}
-		}); 
-		}
-}
-)
+		});
+	}
+})
